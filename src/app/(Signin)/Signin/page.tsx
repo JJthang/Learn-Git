3ce7df404ab03a10db7c03ed1c24ecd763d87cc6
thangpd <thangpd@smartosc.com>
2024-03-05 14:22:24 +0700

    Complete Contact Page

---
+++ src/app/(Signin)/Signin/page.tsx
@@ -0,0 +1,28 @@
+'use client+'
+import React, { useState } from 'react'
+
+type Props = {}
+
+const Signin = (props: Props) => {
+  const [currentState, setCurrentState] = useState(false);
+  const TotalFuction = () => {
+    return (
+      <div>
+        <h1>Hello World </h1>
+      </div>
+    )
+  }
+  return (
+    <div>
+      {
+        currentState ? TotalFuction() : 'hello world'
+      }
+      <label htmlFor="\">Name</label>
+      <input type="text" placeholder='Enter Name' />
+      <label htmlFor="\">Psssword</label>
+      <input type="text" placeholder='Enter Psssword' />
+    </div>
+  )
+}
+
+export default Signin
